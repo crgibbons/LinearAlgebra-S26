85fefa0e3a63617e8a916f5a5d8c9239393f5aa5
Courtney R. Gibbons <crgibbon@hamilton.edu>
2026-01-21 14:57:30 -0500

    Latest build deployed.

---
+++ worksheets/lunr-pretext-search-index.js
@@ -0,0 +1,104 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Courtney Gibbons Department of Mathematics and Statistics Hamilton College  Last Updated:       "
+},
+{
+  "id": "activity-wkst-prelim",
+  "level": "1",
+  "url": "activity-wkst-prelim.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Reading Mathematics",
+  "body": " Reading Mathematics   Reading mathematics, and especially mathematics textbooks, is not like reading a novel for pleasure. Often, one sentence of a book or paper contains a paragraph's worth of information. This worksheet will help you develop some habits to read math effectively.    Advice from the Authors   On page xviii, our textbook authors give advice to a student reader. Summarize the advice here.     Using the End of Section Materials   Head to the back of Section 1.1 and jot down of the key terms. Leave space to write notes about each term. Do this before you start reading the material in the chapter.  How many terms are in your list?     Reading for Meaning   The first term in the Key Terms list is linear equation . Start reading the chapter and keep an eye out for this term.  We find the first appearance of the term on page 1. It is defined as an equation of the form where the variables ( ) are unknowns and and are real (or complex) numbers.  We can see that it is closely related to the next terms in the Key Terms list, solution and linear system . Write those definitions in your list of terms, too. It's helpful to include a page numnber where it appears in the book in case you want to find it again.     Reading Comprehension: Examples and Non-Examples   When you encounter a defintion, it's a good idea to jot down an example and a non-example.  With your team, come up with an example and non-example of a linear system based on page 1 of our textbook. Do not reuse the (non-)examples in the book! Make note of how many equations and how many unknowns are in your examples and non-examples.  Why is your non-example not a linear system? Are the other ways to make a non-example?  Continue reading until Example 1 on page 2. Make sure to fill out the definitions of the Key Terms you encounter.  Before you read Example 1, see how the new terms relate to your example of a linear system. Does your linear system have a solution ? If not, what term should we use to describe it? If so, what would you check next?     Examples in the Textbook   The authors include an example (Example 1) in the textbook on page 2.  What are the authors trying to demonstrate with the example? That is, what's the point of the example and why is it being shown to the reader now?  Write a note for yourself that Example 1, page 2, is showing...      Stamina Time! Keep reading, practicing the habits above.   There is one more key term on page 2; don't miss it  Pages 3-5 contain Examples 2-5. Repeat the previous exercise for each of these examples.  After Example 5, the authors summarize some material and present figures with geometric interpretations. If you have questions about these figures, jot them down so you can ask about them in class (or on Zulip before class).  Finish reading the chapter.    Reading Comprehension: Trying Exercises   Your professor has assigned some homework problems, but she did not assign number 1. Try that with individually and check with your teammates when you all finish.  Your professor assigned problems 8, 12, and 16. Decide which group member will be the lead recorder for each problem (this role should rotate), and talk through the problems together.  If you are stuck as a team, flag down your professor for help!    Key Terms     linear equation  page 1  an equation like where are real (or complex) and are variables. This can have any number of variables and coefficients (not just two).      solution of a linear equation  page      linear system  page      unknowns  page      inconsistent system  page      consistent system  page      homogeneous system  page      trivial solution  page  Note: only applies to homogeneous systems      nontrivial solution  page  Note: only applies to homogeneous systems      equivalent systems  page      unique solution  page      no solution  page      infinitely many solutions  page      manipulations on linear systerms  page      method of elimination  page  See also box on page 6!        Examples and Non-Examples    linear system of equations  example  non-example      "
+},
+{
+  "id": "activity-wkst-prelim-3",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-3",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "Advice from the Authors.",
+  "body": " Advice from the Authors   On page xviii, our textbook authors give advice to a student reader. Summarize the advice here.   "
+},
+{
+  "id": "activity-wkst-prelim-4",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-4",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "Using the End of Section Materials.",
+  "body": " Using the End of Section Materials   Head to the back of Section 1.1 and jot down of the key terms. Leave space to write notes about each term. Do this before you start reading the material in the chapter.  How many terms are in your list?   "
+},
+{
+  "id": "activity-wkst-prelim-5",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-5",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "Reading for Meaning.",
+  "body": " Reading for Meaning   The first term in the Key Terms list is linear equation . Start reading the chapter and keep an eye out for this term.  We find the first appearance of the term on page 1. It is defined as an equation of the form where the variables ( ) are unknowns and and are real (or complex) numbers.  We can see that it is closely related to the next terms in the Key Terms list, solution and linear system . Write those definitions in your list of terms, too. It's helpful to include a page numnber where it appears in the book in case you want to find it again.   "
+},
+{
+  "id": "activity-wkst-prelim-6",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-6",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "Reading Comprehension: Examples and Non-Examples.",
+  "body": " Reading Comprehension: Examples and Non-Examples   When you encounter a defintion, it's a good idea to jot down an example and a non-example.  With your team, come up with an example and non-example of a linear system based on page 1 of our textbook. Do not reuse the (non-)examples in the book! Make note of how many equations and how many unknowns are in your examples and non-examples.  Why is your non-example not a linear system? Are the other ways to make a non-example?  Continue reading until Example 1 on page 2. Make sure to fill out the definitions of the Key Terms you encounter.  Before you read Example 1, see how the new terms relate to your example of a linear system. Does your linear system have a solution ? If not, what term should we use to describe it? If so, what would you check next?   "
+},
+{
+  "id": "activity-wkst-prelim-7",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-7",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "Examples in the Textbook.",
+  "body": " Examples in the Textbook   The authors include an example (Example 1) in the textbook on page 2.  What are the authors trying to demonstrate with the example? That is, what's the point of the example and why is it being shown to the reader now?  Write a note for yourself that Example 1, page 2, is showing...    "
+},
+{
+  "id": "activity-wkst-prelim-8",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-8",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "Stamina Time! Keep reading, practicing the habits above..",
+  "body": " Stamina Time! Keep reading, practicing the habits above.   There is one more key term on page 2; don't miss it  Pages 3-5 contain Examples 2-5. Repeat the previous exercise for each of these examples.  After Example 5, the authors summarize some material and present figures with geometric interpretations. If you have questions about these figures, jot them down so you can ask about them in class (or on Zulip before class).  Finish reading the chapter.   "
+},
+{
+  "id": "activity-wkst-prelim-9",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-9",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "Reading Comprehension: Trying Exercises.",
+  "body": "Reading Comprehension: Trying Exercises   Your professor has assigned some homework problems, but she did not assign number 1. Try that with individually and check with your teammates when you all finish.  Your professor assigned problems 8, 12, and 16. Decide which group member will be the lead recorder for each problem (this role should rotate), and talk through the problems together.  If you are stuck as a team, flag down your professor for help!   "
+},
+{
+  "id": "activity-wkst-prelim-10-2",
+  "level": "2",
+  "url": "activity-wkst-prelim.html#activity-wkst-prelim-10-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "linear equation solution of a linear equation linear system unknowns inconsistent system consistent system homogeneous system trivial solution nontrivial solution equivalent systems unique solution no solution infinitely many solutions manipulations on linear systerms method of elimination "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
